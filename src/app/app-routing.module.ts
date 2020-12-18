0b19677ada9bae79b8d22ee4ea01485b3e1d4f1a
deaneturner <service@deaneturner.com>
2020-12-18 09:58:55 -0500

    working title service bound to route data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,68 +1,73 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer, NoPreloading} from '@angular/router';
-import {LoginComponent} from './login/login.component';
-import {AboutComponent} from './about/about.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {CanLoadAuthGuard} from './services/can-load-auth.guard';
-import {CustomPreloadingStrategy} from './services/custom-preloading.strategy';
-import {ChatComponent} from './chat/chat.component';
-
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules, UrlSerializer, NoPreloading } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CanLoadAuthGuard } from './services/can-load-auth.guard';
+import { CustomPreloadingStrategy } from './services/custom-preloading.strategy';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
-    {
-      path: "",
-      redirectTo: "/courses",
-        pathMatch: "full"
-    },
-    {
-      path: "courses",
-      loadChildren: () => import('./courses/courses.module')
-                            .then(m => m.CoursesModule),
-        // canLoad: [CanLoadAuthGuard]
-       data: {
-          preload: false
-       }
-    },
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: 'helpdesk-chat',
-        component: ChatComponent,
-        outlet: 'chat'
-    },
-    {
-        path: "**",
-        component: PageNotFoundComponent
+  {
+    path: '',
+    redirectTo: '/courses',
+    pathMatch: 'full'
+  },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module')
+      .then(m => m.CoursesModule),
+    // canLoad: [CanLoadAuthGuard]
+    data: {
+      preload: false,
+      title: 'Courses!'
     }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Component!'
+    }
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'helpdesk-chat',
+    component: ChatComponent,
+    outlet: 'chat',
+    data: {
+      title: 'Chat Component!'
+    }
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 
 ];
 
 @NgModule({
   imports: [
-      RouterModule.forRoot(
-          routes, {
-              preloadingStrategy: CustomPreloadingStrategy,
-              scrollPositionRestoration:'enabled',
-              paramsInheritanceStrategy: 'always',
-              relativeLinkResolution: 'corrected',
-              malformedUriErrorHandler:
-                  (error: URIError, urlSerializer: UrlSerializer, url:string) =>
-                    urlSerializer.parse("/page-not-found")
-          })
+    RouterModule.forRoot(
+      routes, {
+        preloadingStrategy: CustomPreloadingStrategy,
+        scrollPositionRestoration: 'enabled',
+        paramsInheritanceStrategy: 'always',
+        relativeLinkResolution: 'corrected',
+        malformedUriErrorHandler:
+          (error: URIError, urlSerializer: UrlSerializer, url: string) =>
+            urlSerializer.parse('/page-not-found')
+      })
   ],
   exports: [RouterModule],
   providers: [
-      CanLoadAuthGuard,
-      CustomPreloadingStrategy
+    CanLoadAuthGuard,
+    CustomPreloadingStrategy
   ]
 })
 export class AppRoutingModule {
-
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthStore} from './services/auth.store';
-
-
+// TODO: document
+import { filter, map } from 'rxjs/operators';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+// END TODO
 
 @Component({
   selector: 'app-root',
@@ -9,13 +12,36 @@ import {AuthStore} from './services/auth.store';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements  OnInit {
-
-    constructor(public auth: AuthStore) {
-
+    // TODO: document
+    constructor(
+      public auth: AuthStore,
+      private titleService: Title,
+      private router: Router,
+      private activatedRoute: ActivatedRoute) {
     }
+    // END TODO
 
     ngOnInit() {
 
+      // TODO: document, package
+      const appTitle = this.titleService.getTitle();
+      this.router
+        .events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => {
+          let child = this.activatedRoute.firstChild;
+          while (child.firstChild) {
+            child = child.firstChild;
+          }
+          if (child.snapshot.data['title']) {
+            return child.snapshot.data['title'];
+          }
+          return appTitle;
+        })
+      ).subscribe((ttl: string) => {
+        this.titleService.setTitle(ttl);
+      });
+      // END TODO
 
     }
 
